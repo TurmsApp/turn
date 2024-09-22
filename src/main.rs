dd4f726c44af4593f12a67187ea05f1a4c1fff23
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-09-22 15:59:53 +0200

    Remove redundancy

--- src/main.rs
+++ src/main.rs
@@ -5,46 +5,19 @@ mod authentication;
 mod logger;
 mod metrics;
 
-use std::collections::HashMap;
-use std::net::{IpAddr, SocketAddr};
+use std::net::IpAddr;
 use std::str::FromStr;
 use std::sync::Arc;
+use authentication::Authenticator;
 use tokio::net::UdpSocket;
 use tokio::signal;
 use tokio::time::Duration;
 use tracing::info;
-use turn::auth::*;
 use turn::relay::relay_static::*;
 use turn::server::config::*;
 use turn::server::*;
 use turn::Error;
 use webrtc_util::vnet::net::*;
-
-struct MyAuthHandler {
-    cred_map: HashMap<String, Vec<u8>>,
-}
-
-impl MyAuthHandler {
-    fn new(cred_map: HashMap<String, Vec<u8>>) -> Self {
-        MyAuthHandler { cred_map }
-    }
-}
-
-impl AuthHandler for MyAuthHandler {
-    fn auth_handle(
-        &self,
-        username: &str,
-        _realm: &str,
-        _src_addr: SocketAddr,
-    ) -> Result<Vec<u8>, Error> {
-        if let Some(pw) = self.cred_map.get(username) {
-            //log::debug!("username={}, password={:?}", username, pw);
-            Ok(pw.to_vec())
-        } else {
-            Err(Error::ErrFakeErr)
-        }
-    }
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -57,13 +30,13 @@ async fn main() -> Result<(), Error> {
     let port = std::env::var("PORT").unwrap_or("3478".into());
     let realm = std::env::var("REALM").unwrap_or("".into());
 
-    let creds: Vec<&str> = vec!["user=pass"];
+    /*let creds: Vec<&str> = vec!["user=pass"];
     let mut cred_map = HashMap::new();
     for user in creds {
         let cred: Vec<&str> = user.splitn(2, '=').collect();
         let key = generate_auth_key(cred[0], &realm, cred[1]);
         cred_map.insert(cred[0].into(), key);
-    }
+    }*/
 
     let conn = Arc::new(UdpSocket::bind(format!("0.0.0.0:{port}")).await?);
     info!("Listening to UDP on {}", conn.local_addr()?);
@@ -77,8 +50,8 @@ async fn main() -> Result<(), Error> {
                 net: Arc::new(Net::new(None)),
             }),
         }],
-        realm: realm,
-        auth_handler: Arc::new(MyAuthHandler::new(cred_map)),
+        realm,
+        auth_handler: Arc::new(Authenticator::default()),
         channel_bind_timeout: Duration::from_secs(0),
         alloc_close_notify: None,
     })
