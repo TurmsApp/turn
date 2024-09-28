e9e0bcaab0e6ae84be8244e89f8043e61ca1f7c0
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-09-28 21:10:12 +0200

    Read file and jwt key

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,18 @@ mod authentication;
 mod logger;
 mod metrics;
 
+use authentication::string_to_algorithm;
+use authentication::Authenticator;
+use libturms::jwt::Key;
+use std::env;
+use std::fs;
 use std::net::IpAddr;
 use std::str::FromStr;
 use std::sync::Arc;
-use authentication::Authenticator;
 use tokio::net::UdpSocket;
 use tokio::signal;
 use tokio::time::Duration;
-use tracing::info;
+use tracing::*;
 use turn::relay::relay_static::*;
 use turn::server::config::*;
 use turn::server::*;
@@ -26,17 +30,51 @@ async fn main() -> Result<(), Error> {
     // Init Prometheus metrics.
     metrics::register_custom_metrics();
 
-    let public_ip = std::env::var("PUBLIC_IP").unwrap_or("0.0.0.0".into());
-    let port = std::env::var("PORT").unwrap_or("3478".into());
-    let realm = std::env::var("REALM").unwrap_or("".into());
+    let public_ip = env::var("PUBLIC_IP").unwrap_or("0.0.0.0".into());
+    let port = env::var("PORT").unwrap_or("3478".into());
+    let realm = env::var("REALM").unwrap_or("".into());
 
-    /*let creds: Vec<&str> = vec!["user=pass"];
-    let mut cred_map = HashMap::new();
-    for user in creds {
-        let cred: Vec<&str> = user.splitn(2, '=').collect();
-        let key = generate_auth_key(cred[0], &realm, cred[1]);
-        cred_map.insert(cred[0].into(), key);
-    }*/
+    let mut authentificator = Authenticator::default().algorithm(
+        string_to_algorithm(env::var("JWT_ALGORITHM").unwrap_or_default()),
+    );
+
+    if let Ok(path) = env::var("USERS_PATH") {
+        match fs::read_to_string(&path) {
+            Ok(lines) => {
+                for line in lines.lines() {
+                    let line = line.to_string().clone();
+                    let cred: Vec<&str> = line.splitn(2, '=').collect();
+
+                    debug!(username = cred[0], "Added user.");
+                    authentificator.add_user(
+                        cred[0].to_owned(),
+                        cred[1].to_owned(),
+                        &realm,
+                    );
+                }
+            },
+            Err(_) => error!(path = path, "Cannot find file."),
+        }
+    }
+
+    if let Ok(key) = env::var("JWT_PUBLIC_KEY") {
+        // If user directly entered public key.
+        if key.starts_with("-----BEGIN PUBLIC KEY-----") {
+            if authentificator
+                .public_key(Key::<String>::Text(key))
+                .is_err()
+            {
+                error!(
+                    "JWT key is not valid; make sure you used the public key."
+                )
+            }
+        } else {
+            // However, it MUST be a path.
+            if authentificator.public_key(Key::Path(key)).is_err() {
+                error!("JWT key is not valid: is the path valid? is it the public key?")
+            }
+        }
+    }
 
     let conn = Arc::new(UdpSocket::bind(format!("0.0.0.0:{port}")).await?);
     info!("Listening to UDP on {}", conn.local_addr()?);
@@ -51,7 +89,7 @@ async fn main() -> Result<(), Error> {
             }),
         }],
         realm,
-        auth_handler: Arc::new(Authenticator::default()),
+        auth_handler: Arc::new(authentificator),
         channel_bind_timeout: Duration::from_secs(0),
         alloc_close_notify: None,
     })
