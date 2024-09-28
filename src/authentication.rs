cd8b701a21b5ce4e3c6bbbfc4a3ea6d72b614d2e
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-09-28 21:10:35 +0200

    Add JWT support for authentification (#1)
    * Add `libturms` dependency
    
    * Add authentification logic
    
    * Remove redundancy
    
    * Read file and jwt key

--- src/authentication.rs
+++ src/authentication.rs
@@ -1,1 +1,97 @@
+//! Handle authentification logic.
 
+use libturms::error::Error as TurmsError;
+use libturms::jwt::*;
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::path::Path;
+use turn::auth::*;
+use turn::Error;
+
+pub fn string_to_algorithm(algo: String) -> Algorithm {
+    match algo.to_uppercase().as_str() {
+        "ES256" => Algorithm::ES256,
+        "ES384" => Algorithm::ES384,
+        "EDDSA" => Algorithm::EdDSA,
+        "HS256" => Algorithm::HS256,
+        "HS384" => Algorithm::HS384,
+        "HS512" => Algorithm::HS512,
+        "PS256" => Algorithm::PS256,
+        "PS384" => Algorithm::PS384,
+        "PS512" => Algorithm::PS512,
+        "RS256" => Algorithm::RS256,
+        "RS384" => Algorithm::RS384,
+        "RS512" => Algorithm::RS512,
+        _ => Algorithm::RS256,
+    }
+}
+
+/// Authentication management.
+/// Checks connection information.
+#[derive(Default)]
+pub struct Authenticator {
+    /// Username and password combo.
+    pub combo: HashMap<String, Vec<u8>>, // fight!
+    token_manager: Option<TokenManager>,
+    /// Allow only specified IPs to connect.
+    /// Leave empty to allow all IPs.
+    allowed_ips: Vec<SocketAddr>,
+}
+
+impl Authenticator {
+    /// Allow usage of jsonwebtoken to connect.
+    pub fn public_key<P: AsRef<Path>>(
+        &mut self,
+        key: Key<P>,
+    ) -> Result<(), TurmsError> {
+        self.token_manager = Some(TokenManager::new(None, key)?);
+        Ok(())
+    }
+
+    /// Update algorithm used by JWT.
+    pub fn algorithm(mut self, algorithm: Algorithm) -> Self {
+        if let Some(manager) = self.token_manager {
+            self.token_manager = Some(manager.algorithm(algorithm));
+        }
+        self
+    }
+
+    /// Insert a new combo user-password.
+    pub fn add_user(
+        &mut self,
+        username: String,
+        password: String,
+        realm: &str,
+    ) {
+        let key = generate_auth_key(&username, realm, &password);
+        self.combo.insert(username, key);
+    }
+}
+
+impl AuthHandler for Authenticator {
+    fn auth_handle(
+        &self,
+        username: &str,
+        _realm: &str,
+        src_addr: SocketAddr,
+    ) -> Result<Vec<u8>, Error> {
+        // Check user IP.
+        if !self.allowed_ips.is_empty()
+            && self.allowed_ips.iter().any(|ip| ip.ip() != src_addr.ip())
+        {
+            return Err(Error::ErrPeerAddressFamilyMismatch);
+        }
+
+        if let Some(manager) = &self.token_manager {
+            let _claims = manager.decode(username).map_err(|_error| {
+                Error::Other("Perhaps your token has expired?".into())
+            })?;
+
+            Ok(Vec::new())
+        } else if let Some(pw) = self.combo.get(username) {
+            Ok(pw.clone())
+        } else {
+            Err(Error::ErrClosed)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,46 +5,23 @@ mod authentication;
 mod logger;
 mod metrics;
 
-use std::collections::HashMap;
-use std::net::{IpAddr, SocketAddr};
+use authentication::string_to_algorithm;
+use authentication::Authenticator;
+use libturms::jwt::Key;
+use std::env;
+use std::fs;
+use std::net::IpAddr;
 use std::str::FromStr;
 use std::sync::Arc;
 use tokio::net::UdpSocket;
 use tokio::signal;
 use tokio::time::Duration;
-use tracing::info;
-use turn::auth::*;
+use tracing::*;
 use turn::relay::relay_static::*;
 use turn::server::config::*;
 use turn::server::*;
 use turn::Error;
 use webrtc_util::vnet::net::*;
-
-struct MyAuthHandler {
-    cred_map: HashMap<String, Vec<u8>>,
-}
-
-impl MyAuthHandler {
-    fn new(cred_map: HashMap<String, Vec<u8>>) -> Self {
-        MyAuthHandler { cred_map }
-    }
-}
-
-impl AuthHandler for MyAuthHandler {
-    fn auth_handle(
-        &self,
-        username: &str,
-        _realm: &str,
-        _src_addr: SocketAddr,
-    ) -> Result<Vec<u8>, Error> {
-        if let Some(pw) = self.cred_map.get(username) {
-            //log::debug!("username={}, password={:?}", username, pw);
-            Ok(pw.to_vec())
-        } else {
-            Err(Error::ErrFakeErr)
-        }
-    }
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -53,16 +30,50 @@ async fn main() -> Result<(), Error> {
     // Init Prometheus metrics.
     metrics::register_custom_metrics();
 
-    let public_ip = std::env::var("PUBLIC_IP").unwrap_or("0.0.0.0".into());
-    let port = std::env::var("PORT").unwrap_or("3478".into());
-    let realm = std::env::var("REALM").unwrap_or("".into());
+    let public_ip = env::var("PUBLIC_IP").unwrap_or("0.0.0.0".into());
+    let port = env::var("PORT").unwrap_or("3478".into());
+    let realm = env::var("REALM").unwrap_or("".into());
 
-    let creds: Vec<&str> = vec!["user=pass"];
-    let mut cred_map = HashMap::new();
-    for user in creds {
-        let cred: Vec<&str> = user.splitn(2, '=').collect();
-        let key = generate_auth_key(cred[0], &realm, cred[1]);
-        cred_map.insert(cred[0].into(), key);
+    let mut authentificator = Authenticator::default().algorithm(
+        string_to_algorithm(env::var("JWT_ALGORITHM").unwrap_or_default()),
+    );
+
+    if let Ok(path) = env::var("USERS_PATH") {
+        match fs::read_to_string(&path) {
+            Ok(lines) => {
+                for line in lines.lines() {
+                    let line = line.to_string().clone();
+                    let cred: Vec<&str> = line.splitn(2, '=').collect();
+
+                    debug!(username = cred[0], "Added user.");
+                    authentificator.add_user(
+                        cred[0].to_owned(),
+                        cred[1].to_owned(),
+                        &realm,
+                    );
+                }
+            },
+            Err(_) => error!(path = path, "Cannot find file."),
+        }
+    }
+
+    if let Ok(key) = env::var("JWT_PUBLIC_KEY") {
+        // If user directly entered public key.
+        if key.starts_with("-----BEGIN PUBLIC KEY-----") {
+            if authentificator
+                .public_key(Key::<String>::Text(key))
+                .is_err()
+            {
+                error!(
+                    "JWT key is not valid; make sure you used the public key."
+                )
+            }
+        } else {
+            // However, it MUST be a path.
+            if authentificator.public_key(Key::Path(key)).is_err() {
+                error!("JWT key is not valid: is the path valid? is it the public key?")
+            }
+        }
     }
 
     let conn = Arc::new(UdpSocket::bind(format!("0.0.0.0:{port}")).await?);
@@ -77,8 +88,8 @@ async fn main() -> Result<(), Error> {
                 net: Arc::new(Net::new(None)),
             }),
         }],
-        realm: realm,
-        auth_handler: Arc::new(MyAuthHandler::new(cred_map)),
+        realm,
+        auth_handler: Arc::new(authentificator),
         channel_bind_timeout: Duration::from_secs(0),
         alloc_close_notify: None,
     })
